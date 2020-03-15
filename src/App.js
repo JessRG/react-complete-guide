c221b4c988eb95c6915cadb94d2d8bd010dede82
Jesus Rogelio Garza <jesus.r.g27@gmail.com>
2020-03-15 18:24:12 -0500

    Added in component properties, state, and a button to handle a click event within the App component. Used the props and child prop within the Person component. Added in some more detailed comments.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  /** Understanding and using state
+   *
+   * State managed from inside a component state property is only available like this in classes/components that extend Component
+   *
+   * state -> reserved keyword that is only available in class-based react components
+   *
+   * react 16.8 -> React hooks feature was released which can also manage state and functional components */
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
+
+  // Manipulating the State
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    //this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
   render() {
     return (
-      //JSX
+      // JSX
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        {/* Understanding and using state */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* Working with Properties
+            -> props allow you to pass data from a parent (wrapping) component to a child (embedded) component (passes data down the component tree)
+            -> state is used to change the component state from within
+            -> state implemented with "this" keyword
+            -> changes in props  and/or state  trigger React to re-render your components and potentially update the DOM in the browser
+        */}
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        {/* Understanding the "children" Prop */}
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 // The arrow function holds some advantages especially when it comes to the "this" keyword
-const person = () => {
+// You should use function components as often as possible because state must be handled with care
+// Having state in all your components and manipulating it from anywhere within your app will make the app quickly unpredictable and hard to manage especially as it grows
+const person = props => {
   return (
     // Outputting Dynamic Content
-    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    // <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    <div>
+      {/* Working with Properties */}
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      {/* Understanding the "children" Prop */}
+      <p>{props.children}</p>
+    </div>
   );
 };
 
