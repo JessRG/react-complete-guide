8373021d5b307e2424657570b08e9a4683290835
Jesus Rogelio Garza <jesus.r.g27@gmail.com>
2020-03-17 13:08:31 -0500

    added example of state manipulation with a react hook using useState() to add functionality to functional components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
   /** Understanding and using state
    *
-   * State managed from inside a component state property is only available like this in classes/components that extend Component
-   *
-   * state -> reserved keyword that is only available in class-based react components
-   *
    * react 16.8 -> React hooks feature was released which can also manage state and functional components */
-  state = {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value'
-  };
+    ]
+  });
 
-  // Manipulating the State
-  switchNameHandler = () => {
+  // update old state by calling useState a second time
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  // // Manipulating the State
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
-    //this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    // using setPersonsState does NOT merge whatever you pass to it with the old state, instead it replaces the old state
+    // must manually include all old state data when updating the state OR call useState multiple times
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
@@ -32,43 +34,36 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      // JSX
-      <div className='App'>
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        {/* Understanding and using state */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        {/* Working with Properties
+  return (
+    // JSX
+    <div className='App'>
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      {/* Understanding and using state */}
+      <button onClick={switchNameHandler}>Switch Name</button>
+      {/* Working with Properties
             -> props allow you to pass data from a parent (wrapping) component to a child (embedded) component (passes data down the component tree)
             -> state is used to change the component state from within
             -> state implemented with "this" keyword
             -> changes in props  and/or state  trigger React to re-render your components and potentially update the DOM in the browser
         */}
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        {/* Understanding the "children" Prop */}
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-    // return React.createElement(
-    //   'div',
-    //   null,
-    //   React.createElement('h1', {className: 'App'}, 'Does this work now?')
-    // );
-  }
-}
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      {/* Understanding the "children" Prop */}
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
-export default App;
+export default app;
