f713c8725085c60ecb6acbc7d05a8a33b27dea20
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-11-27 04:17:00 -0600

    Added an authenticated property to the state, passed that authenticated value as a property of the persons components to each person, and a new log in button to the Cockpit component. Intro to Context in React

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -29,6 +29,7 @@ class Person extends Component {
 
 		return (
 			<Aux>
+				{this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
 				<p onClick={this.props.click}>
 					I am {this.props.name} and I am {this.props.age} years old!
 				</p>
